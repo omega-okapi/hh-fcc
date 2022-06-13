940b6558dad2e59bb96d52fa2c82c21302b8579a
amicable-alpaca <amicablealpaca@protonmail.com>
2022-06-13 16:16:48 -0400

    at tx receipt vs tx part

--- ethers-simple-storage-fcc/deploy.js
+++ ethers-simple-storage-fcc/deploy.js
@@ -1,13 +1,36 @@
 const ethers = require("ethers");
+const fs = require("fs");
 
 // http://127.0.0.1:7545
 
-function main() {
+async function main() {
   console.log("hi");
 
   let variable = 5;
 
   console.log(variable);
+
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+
+  const wallet = new ethers.Wallet(
+    "f8401ce4c7a9ef543d9e046c0de9e0f4c31353825477d034e7a687d3763b47ff",
+    provider
+  );
+
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  const contract = await contractFactory.deploy();
+  console.log("Here is the transaction:");
+  console.log(contract.deployTransaction);
+  console.log("Here is the receipt:");
+  console.log(transactionReceipt);
 }
 
 main()
